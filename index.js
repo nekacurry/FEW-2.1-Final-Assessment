ec45ec052b96d43645d410a17aedee27801d8b7f
nekacurry <anneka498@gmail.com>
2021-05-12 18:46:17 -0500

    redid challenge 1 with str lib

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
+var strLib = require('anneka-string-lib');
 var data = require('./data.json');
-// Helpers
-// Capitalize first letter of string
-function capitalize(str) {
-    if (str != '') {
-        var first = str[0].toUpperCase();
-        var rest = str.slice(1);
-    }
-    else {
-        return '';
-    }
-    return first + rest;
-}
-// Capitalize first letter of each word
-function capitalizeWords(str) {
-    var words = str.split(' ');
-    var upperWords = words.map(function (word) { return capitalize(word); });
-    return upperWords.join(' ');
-}
 // Challenges
 function printData() {
-    console.log(capitalizeWords(data[0].first_name) + " " + capitalizeWords(data[0].last_name));
+    console.log(strLib.capitalizeWords(data[0].first_name) + " " + strLib.capitalizeWords(data[0].last_name));
 }
 printData();
--- index.ts
+++ index.ts
@@ -1,31 +1,11 @@
+const strLib = require('anneka-string-lib');
 const data = require('./data.json');
-
-
-// Helpers
-
-// Capitalize first letter of string
-function capitalize(str: string) {
-  if (str != ''){
-    var first: string = str[0].toUpperCase()
-    var rest: string = str.slice(1)
-  } else {
-    return ''
-  }
-  return first + rest
-}
-
-// Capitalize first letter of each word
-function capitalizeWords(str: string) {
-  const words: string[] = str.split(' ')
-  const upperWords: string[] = words.map( word => capitalize(word) )
-  return upperWords.join(' ')
-}
 
 
 // Challenges
 
 function printData() {
-  console.log(`${capitalizeWords(data[0].first_name)} ${capitalizeWords(data[0].last_name)}`)
+  console.log(`${strLib.capitalizeWords(data[0].first_name)} ${strLib.capitalizeWords(data[0].last_name)}`)
 }
 
 printData()
